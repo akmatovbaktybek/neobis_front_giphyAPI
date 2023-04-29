b0d5f6cfeb9ba7306de84527c525c405bdfac10d
akmatovbaktybek <byakmatov@gmail.com>
2023-04-29 12:45:55 +0600

    second commit

--- js/scipt.js
+++ js/scipt.js
@@ -1,20 +1,25 @@
-const searchInput = document.querySelector('#search');
 const gifsContainer = document.querySelector('#gifs');
+const form = document.querySelector('form');
+const searchInput = document.querySelector('#search');
 
-const API_TRENDING_GIFS = "https://api.giphy.com/v1/gifs/trending?api_key=TFbA9Tcoe4RyKaeFZoZxx2QweATyMsnb&limit=25&rating=g";
 const API_KEY = 'TFbA9Tcoe4RyKaeFZoZxx2QweATyMsnb';
+const API_TRENDING_GIFS = "https://api.giphy.com/v1/gifs/trending?api_key=TFbA9Tcoe4RyKaeFZoZxx2QweATyMsnb&limit=25&rating=g";
+const API_SEARCH_GIFS = "https://api.giphy.com/v1/gifs/search?api_key=TFbA9Tcoe4RyKaeFZoZxx2QweATyMsnb&q=";
 
-getGif();
+getGif(API_TRENDING_GIFS);
 
-async function getGif() {
-   const gifsResponse = await fetch(API_TRENDING_GIFS);
+async function getGif(url) {
+   const gifsResponse = await fetch(url);
    const gifsData = await gifsResponse.json();
 
    showGifs(gifsData);
-   console.log(gifsData);
 }
 
 function showGifs(gifData) {
+
+   //очищаем гифки
+   gifsContainer.innerHTML = '';
+
    gifData.data.forEach((gif) => {
       const gifItem = document.createElement('div');
       gifItem.classList.add('main__gif');
@@ -22,6 +27,17 @@ function showGifs(gifData) {
       <img class="main__gif-img" src="${gif.images.original.url}" alt="${gif.title}">
       `;
       gifsContainer.appendChild(gifItem);
-
    })
 }
+
+form.addEventListener('submit', (e) => {
+   e.preventDefault();
+
+   const apiSearchUrl = `${API_SEARCH_GIFS}${searchInput.value}`;
+   if (searchInput.value) {
+      getGif(apiSearchUrl);
+
+      searchInput.value = '';
+   }
+
+})
